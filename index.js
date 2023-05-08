c43f23275531119d2bcb4b9d7f7431796909f92f
换个头像心好累 <zenonux@outlook.com>
2023-05-08 20:51:19 +0800

    feat: add router params

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 function isUndefined(value) {
-    return value === undefined;
+  return value === undefined
 }
 
-function evaluateGuards(guards, to, from, next) {
-    const guardsLeft = guards.slice(0); // clone the array so we do not accidentally modify it
-    const nextGuard = guardsLeft.shift();
+function evaluateGuards(router, guards, to, from, next) {
+  const guardsLeft = guards.slice(0) // clone the array so we do not accidentally modify it
+  const nextGuard = guardsLeft.shift()
 
-    if (isUndefined(nextGuard)) {
-        next();
-        return;
+  if (isUndefined(nextGuard)) {
+    next()
+    return
+  }
+
+  nextGuard(router, to, from, (nextArg) => {
+    if (isUndefined(nextArg)) {
+      evaluateGuards(router, guardsLeft, to, from, next)
+      return
     }
 
-    nextGuard(to, from, (nextArg) => {
-        if (isUndefined(nextArg)) {
-            evaluateGuards(guardsLeft, to, from, next);
-            return;
-        }
-
-        next(nextArg);
-    });
+    next(nextArg)
+  })
 }
 
-module.exports = function(guards) {
-    if (!Array.isArray(guards)) {
-        throw new Error('You must specify an array of guards');
-    }
+module.exports = function (router, guards) {
+  if (!Array.isArray(guards)) {
+    throw new Error('You must specify an array of guards')
+  }
 
-    return (to, from, next) => {
-        return evaluateGuards(guards, to, from, next);
-    };
+  return (router, to, from, next) => {
+    return evaluateGuards(router, guards, to, from, next)
+  }
 }
