ae9cd0998f929043919616897fa67d5433ecbe4a
ZenoQi <zenonux@outlook.com>
2023-05-09 09:42:30 +0800

    fix: typings

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,18 @@
-import { NavigationGuard } from 'vue-router';
+import type {
+  Router,
+  RouteLocationNormalized,
+  NavigationGuardNext,
+} from "vue-router";
+interface CustonGuard {
+  (
+    router: Router,
+    to: RouteLocationNormalized,
+    from: RouteLocationNormalized,
+    next: NavigationGuardNext
+  ): NavigationGuardNext | Promise<NavigationGuardNext>;
+}
 
-export default function Multiguard(guards: Array<NavigationGuard>): NavigationGuard;
+export default function Multiguard(
+  router: Router,
+  guards: Array<CustonGuard>
+): CustonGuard;
